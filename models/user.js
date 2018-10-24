884a48eb5bccfb93b1b77abab1473585a8ec9dff
Johan Brunet <brunet.johan.23@gmail.com>
2018-10-24 12:34:26 +0200

    [add] Username and email unicity verification

--- models/user.js
+++ models/user.js
@@ -2,15 +2,49 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    email: String,
-    password: String,
-    loginType: {type: String, enum: ['classic', 'ldap', 'oauth']},
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    loginType: {type: String, enum: ['classic', 'ldap', 'oauth'], default: 'classic'},
     boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }],
     createdAt: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
+
+UserSchema.pre('save', async function(next) {
+    const user = await mongoose.model('User', UserSchema).findOne({email: this.email});
+    if(user) {
+        let err = new Error("This email already exists!")
+        err.statusCode = 400
+        throw err
+    }
+    next()
+})
+
+UserSchema.pre('save', async function(next) {
+    const user = await mongoose.model('User', UserSchema).findOne({username: this.username});
+    if(user) {
+        let err = new Error("This username is already taken!")
+        err.statusCode = 400
+        throw err
+    }
+    next()
+})
 
 UserSchema.options.toJSON = {
     transform: function(doc, ret, options) {
