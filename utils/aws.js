033c9c4c4a68b9861259e24b50f7fb425898192e
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-05 14:04:02 +0100

    [update] S3 file upload working

--- utils/aws.js
+++ utils/aws.js
@@ -1,13 +1,11 @@
 const AWS = require('aws-sdk')
 const uuid = require('uuid')
-const fs = require('fs')
-const util = require('util')
-// Convert fs.readFile into Promise version of same    
-const readFile = util.promisify(fs.readFile)
 const throwError = require('./throwError')
 
+// TODO: add Muter for file upload from client
+
 // Create unique bucket name
-const bucketName = process.env.AWS_S3_BUCKET_NAME || 'prellone-' + uuid.v4()
+const bucketName = process.env.AWS_S3_BUCKET_NAME
 
 // Create new S3 connexion
 const s3 = new AWS.S3({
@@ -15,30 +13,39 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-// Create a bucket on S3
-// module.exports.createBucket = async() => {
-//     try {
-//         await s3.createBucket({Bucket: bucketName}).promise()
-//         console.log(`Bucket ${bucketName} successfully created`)
-//         return bucketName
-//     }
-//     catch(error) {
-//         console.error(error)
-//     }
-// }
-
 module.exports.uploadFile = async(fileName) => {
     try {
         const fileContent = await readFile(fileName)
-        const key = `${fileName.split('.')[0]}-${uuid.v4()}`
+        const key = `${fileName}-${uuid.v4()}`
         const params = {
             Bucket: bucketName, // pass your bucket name
             Key: key, // file will be saved as <bucketName>/<fileName>
             Body: JSON.stringify(fileContent, null, 4)
         };
         const uploaded = await s3.upload(params).promise()
-        console.log(`File uploaded successfully at ${uploaded.data.Location}`)
-        return uploaded.data.Location
+        console.log(`File uploaded successfully at ${uploaded.Key}`)
+        return uploaded.Key
+    } 
+    catch (error) {
+        throwError(500, error.message)
+    }
+}
+
+module.exports.uploadProfilePicture = async(fileName, fileContent, directory = null) => {
+    try {
+        const bucket = directory ? `${bucketName}/${directory}` : bucketName
+        const [name, extension] = fileName.split(".")
+        const key = `${name}-${uuid.v4()}.${extension}`
+        console.log(key)
+        const params = {
+            Bucket: bucket, // pass your bucket name
+            Key: key, // file will be saved as <bucketName>/<fileName>
+            ACL: "public-read", // for the picture to be accessible via URL
+            Body: JSON.stringify(fileContent, null, 4)
+        };
+        const uploaded = await s3.upload(params).promise()
+        console.log(`File uploaded successfully at ${uploaded.Location}`)
+        return uploaded.Location
     } 
     catch (error) {
         throwError(500, error.message)
