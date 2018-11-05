0bc2140a224368b3d03100549afcfcd442452633
Unknown <alex.bottero@hotmail.fr>
2018-11-05 15:52:27 +0100

    avancement seeder reste

--- utils/initDB.js
+++ utils/initDB.js
@@ -48,7 +48,157 @@ seedCollections = async() => {
     board2 = {
         title: 'Marion Board',
         lists: ['list1']
-    }
+    },
+    task1={
+            id: 'task1', 
+                index: 0,
+                title:'Make the report',
+                    description: 'You need to make the report in English',
+                    memberIds: ['user1','user2'],
+                    labelIds: ['label1','label2'],
+                    comments: [
+                        {
+                            id: 'comment1',
+                            content: 'This should be easy to do !',
+                            userId: 'user1'
+                        },
+                        {
+                            id: 'comment2',
+                            content: 'Yes !',
+                            userId: 'user2'
+                    },
+                    ],
+                    dueDate: '2018-11-01T18:00:00+01:00',
+                creation_date: null
+        },
+       task2= {
+           id: 'task2',
+            index: 1, 
+            title:'Make the presentation',
+            description: 'You need to make the presentation in English',
+            membersIds: [],
+            labelIds: ['label1','label2'],
+            comments: [],
+            creation_date: null
+        },
+        task3= {
+                 id: 'task3',
+                 index: 0, 
+                 title:'Add a list on board',
+                 description: 'You need to add a list on board with a button. The button stays in the right of the board.',
+                 members: [],
+                 labelIds: ['label1','label2'],
+                 comments: [],
+                 dueDate: '2018-11-15T18:00:00+01:00',
+                creation_date: null
+           },
+           task4= {
+            id: 'task4',
+            index: 1, 
+            title:'Add a card on list',
+            description: '',
+            membersIds: [],
+            labelIds: ['label4','label2','label1'],
+            comments: [],
+            dueDate: '2018-11-01T18:00:00+01:00',
+            creation_date: null,
+            done: false
+        },
+        task5={
+            id: 'task5',
+            index: 0, 
+            title:'Make the tests',
+            description: '',
+            membersIds: [],
+            labelIds: [],
+            comments: [],
+            dueDate: '2018-11-01T18:00:00+01:00',
+            creation_date: null,
+            done: false
+        },
+        task6={
+            id: 'task6',
+            index: 1, 
+            title:'Read the documentation',
+            description: '',
+            membersIds: [],
+            labelIds: ['label1'],
+            comments: [],
+            dueDate: '2018-11-01T18:00:00+01:00',
+            creation_date: null,
+            done: false
+        },
+        task7= {
+            id: 'task7',
+            index: 2, 
+            title:'Read the courses',
+            description: '',
+            membersIds: [],
+            labelIds: ['label1','label3'],
+            comments: [],
+            dueDate: '2018-11-01T18:00:00+01:00',
+            creation_date: null,
+            done: false
+        },
+        list1= {
+            id: 'list1',
+            title:'Todo',
+            cardIds: ['task1', 'task2'],
+            toggleAddCard: false
+        },
+        list2= {
+            id: 'list2',
+            title:'In progress',
+            cardIds: ['task3', 'task4'],
+            toggleAddCard: false
+        },
+        list3={
+            id: 'list3',
+            title:'Done',
+            cardIds: ['task5', 'task6', 'task7'],
+            toggleAddCard: false
+        },
+
+        label1= {
+            id: 'label1',
+            title: 'Optional',
+            color: 'primary'
+        },
+        label2={
+            id: 'label2',
+            title: 'Low',
+            color: 'success'
+        },
+        label3={
+            id: 'label3',
+            title: 'Urgent',
+            color: 'danger'
+        },
+        label4= {
+            id: 'label4',
+            title: 'Important',
+            color: 'warning'
+        },
+        Team1={
+            id:'Team1',
+            name:'lala',
+            boards:['board1'],
+            members:[],
+            toggleAddMember:false
+        },
+        Team2={
+            id:'Team2',
+            name:'Teamzer',
+            boards:['board1','board2'],
+            members:['user2'],
+            toggleAddMember:false
+        },
+        home=['board1','board2'],
+        teamsManager=['Team1','Team2']
+        
+
+
+
 }
 
 
