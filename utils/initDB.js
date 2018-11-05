7b59b72bbbfec118f28eeca00188d135b2fda1ad
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-05 22:51:25 +0100

    [fix] Card and Team seeders

--- utils/initDB.js
+++ utils/initDB.js
@@ -31,6 +31,7 @@ module.exports.initDB = async () => {
             }
             catch(error) {
                 console.error("Seeding database failed")
+                console.error(error)
             }
         }
     })
@@ -71,7 +72,9 @@ seedCollections = async() => {
         profilePicture: 'https://prellone.s3.amazonaws.com/johan/lapin-c2b35fd3-1d16-4572-88ca-cfcbad6d17c2.jpeg'
     }
 
-    const [u1, u2] = await models.user.insertMany([user1, user2])
+    // TODO: create documents
+    const u1 = await models.user.create(user1)
+    const u2 = await models.user.create(user2)
     console.log("Users created")
 
     /**
@@ -88,7 +91,8 @@ seedCollections = async() => {
         owner: u2._id
     }
 
-    const [b1, b2] = await models.board.insertMany([board1, board2])
+    const b1 = await models.board.create(board1)
+    const b2 = await models.board.create(board2)
     console.log("Boards created")
     await models.user.findOneAndUpdate({id: u1._id}, { $push: {boards: {$each: [b1._id, b2._id]} } })
     await models.user.findOneAndUpdate({id: u2._id}, { $push: {boards: {$each: [b1._id, b2._id]} } })
@@ -109,7 +113,9 @@ seedCollections = async() => {
         board: b2._id,
     }
 
-    const [li1, li2, li3] = await models.list.insertMany([list1, list2, list3])
+    const li1 = await models.list.create(list1)
+    const li2 = await models.list.create(list2)
+    const li3 = await models.list.create(list3)
     console.log("Lists created")
     await models.board.findOneAndUpdate({id: b1._id}, { $push: {lists: {$each: [li1._id, li2._id]} } })
     await models.board.findOneAndUpdate({id: b2._id}, { $push: {lists: li3._id } })
@@ -137,7 +143,10 @@ seedCollections = async() => {
         board: b1._id,
         color: 'warning'
     }
-    const [l1b1, l2b1, l3b1, l4b1] = await models.label.insertMany([label1board1, label2board1, label3board1, label4board1])
+    const l1b1 = await models.label.create(label1board1)
+    const l2b1 = await models.label.create(label2board1)
+    const l3b1 = await models.label.create(label3board1)
+    const l4b1 = await models.label.create(label4board1)
     console.log("Labels board1 created")
     
     label1board2 = {
@@ -159,7 +168,10 @@ seedCollections = async() => {
         board: b2._id,
         color: 'warning'
     }
-    const [l1b2, l2b2, l3b2, l4b2] = await models.label.insertMany([label1board2, label2board2, label3board2, label4board2])
+    const l1b2 = await models.label.create(label1board2)
+    const l2b2 = await models.label.create(label2board2)
+    const l3b2 = await models.label.create(label3board2)
+    const l4b2 = await models.label.create(label4board2)
     console.log("Labels board2 created")
 
     await models.board.findOneAndUpdate({id: b1._id}, { $push: {labels: {$each: [l1b1._id, l2b1._id, l3b1._id, l4b1._id]} } })
@@ -227,8 +239,14 @@ seedCollections = async() => {
         labels: [l1b2._id, l3b2._id],
         dueDate: '2018-11-01T18:00:00+01:00'
     }
-    const array = [card1, card2, card3, card4, card5, card6, card7]
-    const [c1, c2, c3, c4, c5, c6, c7] = await models.card.insertMany(array)
+
+    const c1 = await models.card.create(card1)
+    const c2 = await models.card.create(card2)
+    const c3 = await models.card.create(card3)
+    const c4 = await models.card.create(card4)
+    const c5 = await models.card.create(card5)
+    const c6 = await models.card.create(card6)
+    const c7 = await models.card.create(card7)
     console.log("Cards created")
 
     await models.list.findOneAndUpdate({id: li1._id}, { $push: {cards: {$each: [c1._id, c2._id]} } })
@@ -251,7 +269,8 @@ seedCollections = async() => {
         members: [u2._id]
     }
 
-    const [t1, t2] = await models.team.insertMany([team1, team2])
+    const t1 = await models.team.create(team1)
+    const t2 = await models.team.create(team2)
     console.log("Teams created")
 
     await models.user.findOneAndUpdate({id: u1._id}, { $push: {teams: t1._id } })
