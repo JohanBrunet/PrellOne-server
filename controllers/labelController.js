163ea2732f3dbf443e073bc4038ca6511276e5ce
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-06 10:08:56 +0100

    [add] Labels controller

---
+++ controllers/labelController.js
@@ -0,0 +1,36 @@
+const Label = require('../models/label')
+const mongoose = require('mongoose')
+const throwError = require('../utils/throwError')
+
+let LabelController = () => {}
+
+LabelController.getByID = (id) => {
+    return Label.findById(id)
+}
+
+LabelController.getAll = (boardId) => {
+    return Label.find({board: boardId})
+}
+
+LabelController.create = async(boardId, color, title = null) => {
+    const newLabel = new Label({
+        board: boardId,
+        color: color
+    })
+    if (title) newLabel.title = title
+    try {
+        return await newLabel.save()
+    }
+    catch(error) {
+        throw error
+    }
+}
+
+LabelController.updateTitle = (labelId, title) => {
+    const query = {'id': labelId};
+    const update = {$set: {'title': title}}
+    const options = {new: true, upsert: true};
+    return Board.findOneAndUpdate(query, update, options);
+}
+
+module.exports = LabelController;
