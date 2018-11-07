20691e5f4b2d37cd2269932e87d9cb49e6eecdb2
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-07 23:23:42 +0100

    [update] Register no longer return user

--- routes/auth.js
+++ routes/auth.js
@@ -5,23 +5,20 @@ const userController = require('../controllers/userController');
 
     
     router.post('/login', asyncWrapper( async(req, res, next) => {
-        const result = await authenticate(req.body.email, req.body.password);
-        let user = await userController.getWithBoards(result.id)
-        user = user.toJSON()
-        delete user.password
+        const result = await authenticate(req.body.email, req.body.password)
 
-        res.cookie('prellone', result);
         res.type('application/json');
         res.status(200);
         return res.json(result);
     }));
 
-    router.post('/signup', asyncWrapper( async(req, res, next) => {
+    router.post('/register', asyncWrapper( async(req, res, next) => {
         let newUser = req.body;
         let newUserSaved = await userController.create(newUser);
         console.log(newUserSaved)
+        res.type('text/html');
         res.status(201)
-        return res.json(newUserSaved)
+        return res.send()
     }));
 
     router.get('/logout', (req, res) => {
