b9e557b6e53b35d5b6a893532cb156067294408d
medihebfaiza <fmohamediheb@gmail.com>
2018-11-13 00:43:49 +0100

    [Add] : Google Drive API Quickstart

--- routes/card.js
+++ routes/card.js
@@ -4,6 +4,35 @@ const router = require('express').Router()
 const auth = require('../middlewares/authMiddleware').isAuthenticated
 const asyncWrapper = require('../middlewares/asyncWrapper')
 const cardController = require('../controllers/cardController')
+const fs = require('fs')
+const google = require('../utils/google')
+
+const content = {
+    installed: {
+      client_id: "741754501698-vobipll9beet35bmoml9ovrcou4i1rbh.apps.googleusercontent.com",
+      project_id: "prello-1542055414931",
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://www.googleapis.com/oauth2/v3/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_secret: "Fj8wdAcxcqKpjFCL0YXQPrwj",
+      redirect_uris: ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
+    }
+}
+
+// Attach Google Drive file 
+router.get('/googleFiles',/* auth, */ (req, res, next) => {
+    // Load client secrets from a local file.
+    //fs.readFile('../config/googleCredentials.json', (err, content) => {
+    //    if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Drive API.
+        console.log(content)
+        google.authorize(content, google.listFiles);
+    //  google.authorize(JSON.parse(content), google.listFiles);
+    //});
+    
+    res.type('application/json')
+    res.status(200)
+})
 
 /* GET ALL CARDS */
 router.get('/', /* auth, */ asyncWrapper( async(req, res, next) => {
