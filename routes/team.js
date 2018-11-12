cb461a2e6ca9c45273bb4aea9967bed7f1156b00
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-12 21:35:33 +0100

    [fix] add team
    Get token from request header

--- routes/team.js
+++ routes/team.js
@@ -23,8 +23,9 @@ router.get('/:id', /* auth, */ asyncWrapper( async(req, res, next) => {
 
 router.post('/', /* auth, */ asyncWrapper( async(req, res, next) => {
     const newTeam=req.body
-    const owner = decodeToken(req.cookies.prellone.appAuthToken)
-    const team = await TeamController.create(newTeam ,owner.id)
+    const token = req.get('Authorization').split(' ')[1]
+    const owner = decodeToken(token)
+    const team = await TeamController.create(newTeam ,owner)
     res.type('application/json')
     res.status(200)
     res.json(team)
