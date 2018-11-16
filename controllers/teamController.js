438c102cf6863051a5696725fd3f867da2d6c65e
Marion Rul <marion.mary@hotmail.fr>
2018-11-16 14:42:11 +0100

    [Edit] check if board exists before adding to team

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -7,9 +7,9 @@ let TeamController = () => {}
 TeamController.addBoard = async(teamId, boardId) => {
     const team = await Team.findById(teamId)
         try {
-
-            team.boards.push(boardId)
-    
+            if (!team.boards.some(oldBoardId => oldBoardId.equals(boardId))){
+                team.boards.push(boardId)
+            }
             team.save()
         }
         catch(error) {
